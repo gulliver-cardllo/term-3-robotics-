83acf79bd28855842afa0afccac68e89b0a2e2e3
gulliver-cardllo <0408428@schoolsnet.act.edu.au>
2026-09-03 09:52:20 +1000

    Update arduino.ino, fix accidental upload of wrong code

--- arduino.ino
+++ arduino.ino
@@ -1,49 +1,87 @@
 #include <QTRSensors.h>
 
-
-
 QTRSensors qtr;
 const uint8_t SensorCount = 4;
-
 uint16_t sensorValues[SensorCount];
+const int button = 8;
+int buttonstate = 0;
+bool buttonflip;
+int frequency = 0;
 
 
+const int S0 = A0;
+const int S1 = A1;
+const int S2 = A2;
+const int S3 = A3;
+const int sensorOut = A4;
+const int OE = A5;
 
-void setup()
+void setup() {
 
-{
-  qtr.setSensorPins((const uint8_t[]){4,5,6,7}, SensorCount);
-  // configure the sensors
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  pinMode(OE, OUTPUT);
+  pinMode(sensorOut, INPUT);
+  digitalWrite(S0, HIGH);
+  digitalWrite(S1, LOW);
+
 
   qtr.setTypeRC();
-
-  qtr.setSensorPins((const uint8_t[]){A0}, SensorCount);
-
+  qtr.setSensorPins((const uint8_t[]){ 7, 6, 5, 4 }, SensorCount);
   Serial.begin(9600);
-
+  Serial.print("Goon ");
+  pinMode(button, INPUT);
 }
+void colour() {
+  digitalWrite(S2, LOW);
+  digitalWrite(S3, LOW);
+  frequency = pulseIn(sensorOut, LOW);
+  Serial.print("R= ");      //printing name
+  Serial.print(frequency);  //printing RED color frequency
+  Serial.print("  ");
+  delay(100);
 
+  digitalWrite(S2, HIGH);
+  digitalWrite(S3, HIGH);
+  frequency = pulseIn(sensorOut, LOW);
+  Serial.print("G= ");      //printing name
+  Serial.print(frequency);  //printing Green color frequency
+  Serial.print("  ");
+  delay(100);
 
-
+  digitalWrite(S2, LOW);
+  digitalWrite(S3, HIGH);
+  frequency = pulseIn(sensorOut, LOW);
+  Serial.print("B= ");      //printing name
+  Serial.print(frequency);  //printing Blue color frequency
+  Serial.println("  ");
+  delay(1000);
+}
 void loop()
 
 {
 
-  // read raw sensor values
+  buttonstate = digitalRead(button);
+  if (buttonstate == HIGH) {
 
-  qtr.read(sensorValues);
-
-
-
-  // print the sensor values as numbers from 0 to 1023, where 0 means maximum
-
-  // reflectance and 1023 means minimum reflectance
-
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(sensorValues[i]);
-    Serial.print('\t');
+    buttonflip = !buttonflip;
+    Serial.println("Button Pressed");
+    delay(200);
+  }
+  if (buttonflip == true) {
+    colour();
+  }
+  else{
+    qtr.read(sensorValues);
+     for (uint8_t i = 0; i < SensorCount; i++) {
+      Serial.print(sensorValues[i]);
+      Serial.print('\t');
   }
   Serial.println();
-  delay(250);
+  delay(500);
+  }
+  
+  
 }
